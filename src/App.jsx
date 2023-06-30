43cc0137e36de71454339515c3d55400c79d7675
Dayana Mariano <dayanaraquelle@gmail.com>
2023-06-30 17:50:20 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Movie Review ⭐️</h1>
+      <p>Welcome to Movie Review, your page to rate movies and read reviews</p>
+      <MyList />
+    </div>
+  )
+}
+
+function MyList() {
+  return (
+    <ul>
+      <li>first movie</li>
+    </ul>
+  );
+}
+export default App
