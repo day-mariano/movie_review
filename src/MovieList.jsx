a35290683b2cfc1abcee2638dba043896dd92e87
Dayana Mariano <dayanaraquelle@gmail.com>
2023-08-17 23:56:25 -0300

    Add hooks

--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -1,8 +1,22 @@
 import MovieItems from './MovieItems'
 
 function MovieList() {
+  const [movies,setMovies] = useState([])
+
+  useEffect(() => {
+    const fetch = require('node-fetch');
+
+    const url = 'https://api.themoviedb.org/3/account/null/favorite/movies?language=en-US&page=1&sort_by=created_at.asc';
+    const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+    fetch(url, options)
+      .then(res => res.json())
+      .then(json => console.log(json))
+      .catch(err => console.error('error:' + err));
+  })
+
   return (
-    <ul>
+    <ul className='List'>
       <MovieItems />
       <MovieItems />
       <MovieItems />
