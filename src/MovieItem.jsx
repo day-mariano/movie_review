454315129a547b85412e4e3442dba9c7ef97d537
Dayana Mariano <dayanaraquelle@gmail.com>
2023-08-26 19:01:51 -0300

    Add movie post

--- src/MovieItem.jsx
+++ src/MovieItem.jsx
@@ -1,11 +1,9 @@
-//import StarRater from './StarRater'
-
 function MovieItem(filme) {
   return (
      <li>
         <h3 className="movieName"> {filme.nome} </h3>
         <h4 className="movieVote"> ⭐️{filme.nota} </h4>
-        {/* <StarRater /> */}
+        <img src={"https://www.themoviedb.org/t/p/w220_and_h330_face" + filme.poster} alt={filme.nome} />
      </li>
   )
 }
