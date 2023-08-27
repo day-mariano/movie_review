ae4ea66e555c821139a12cb7d0ab6a603e3eb29a
Dayana Mariano <dayanaraquelle@gmail.com>
2023-08-27 19:33:02 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <h1>Movie Review 🍿</h1>
-      <h2>Welcome to Movie Review, your page to rate movies and read reviews</h2>
+      <h2>Welcome to Movie Review, your page see to average votes and read reviews</h2>
       <MovieList />
     </div>
   )
