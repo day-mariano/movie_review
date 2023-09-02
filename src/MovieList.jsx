0f46bb23e71adfd8ce55d67519e17f2ded94dd34
Dayana Mariano <dayanaraquelle@gmail.com>
2023-09-01 23:07:35 -0300

    Add link style

--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -1,7 +1,7 @@
 import MovieItem from './MovieItem';
 import './App.css'
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 function MovieList() {
   const [movieList, setMovieList] = useState(null);
@@ -32,7 +32,7 @@ function MovieList() {
         )) : (
           <p>Loading movie list...</p>
         )}
-        <Link to={"MovieList/" + filme.id}>Details</Link>
+        {/* <Link to={"MovieList/" + filme.id}>Details</Link> */}
     </ul>
   );
 }
