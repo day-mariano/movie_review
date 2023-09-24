0a11e18b647ba5eaf4cc8a28c0847219de18800e
Dayana Mariano <dayanaraquelle@gmail.com>
2023-09-24 15:03:52 -0300

    Add button details

--- src/MovieItem.jsx
+++ src/MovieItem.jsx
@@ -5,17 +5,19 @@ function MovieItem(props) {
   const filme = props.filme;
   
   return (
-    <Link to={{
-      pathname: '/movie/' + filme.id,
-    }} state={filme}>
-      <li>
+    <li>
       <img src={"https://www.themoviedb.org/t/p/w220_and_h330_face" + filme.poster_path} alt={filme.title} />
       <div className="movieInfo">
         <h4 className="movieName"> {filme.title} </h4>
         <p className="movieVote"> ⭐️ {filme.vote_average} </p>
+        
+        <Link to={{
+          pathname: '/movie/' + filme.id,
+        }} state={filme}id='buttonDetails'> Details
+        </Link>
       </div>
+      
       </li>
-    </Link>
   )
 }
 
